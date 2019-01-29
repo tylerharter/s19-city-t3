bb9f1415139ee1980bbc7288d8723ba0eecf318c
kwiess <46977812+kwiess@users.noreply.github.com>
2019-01-29 16:40:05 -0600

    Update kwiess.py

--- kwiess.py
+++ kwiess.py
@@ -3,3 +3,4 @@ print("PROGRAM TO CALCULATE THE AREA OF A CIRCLE:")
 radius = float(input("Enter the radius of the circle: "))
 area = math.pi * radius * radius
 print("Area of the circle = {}".format(area))
+print("End of program.")
